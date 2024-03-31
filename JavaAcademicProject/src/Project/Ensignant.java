df62844b68a861f3765922bf643f95aae51f9e63
walid885 <bettaieb88walid@gmail.com>
2024-03-31 03:49:31 +0100

    implemnting the class of the Ensignant class in java and testing it

--- JavaAcademicProject/src/Project/Ensignant.java
+++ JavaAcademicProject/src/Project/Ensignant.java
@@ -1,10 +1,57 @@
 package Project;
 
 public class Ensignant {
+	
+	private int matricule;
+	private String nom ; 
+	private String contact ; 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Ensignant(int matricule, String nom, String contact) {
+	    this.matricule = matricule;
+	    this.nom = nom;
+	    this.contact = contact;
 	}
 
+
+	public int getMatricule() {
+		return matricule;
+	}
+
+
+	public void setMatricule(int matricule) {
+		this.matricule = matricule;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getContact() {
+		return contact;
+	}
+
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+
+	public static void main(String[] args) {
+
+        Ensignant ensignant1 = new Ensignant(1, "John Doe", "johndoe@example.com");
+        Ensignant ensignant2 = new Ensignant(2, "Jane Doe", "janedoe@example.com");
+
+        System.out.println("Ensignant 1: " + ensignant1.getMatricule() + " " + ensignant1.getNom() + " " + ensignant1.getContact());
+        System.out.println("Ensignant 2: " + ensignant2.getMatricule() + " " + ensignant2.getNom() + " " + ensignant2.getContact());
+    }
+
 }
+
+
